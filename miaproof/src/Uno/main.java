75b040723d2dc5aa466726959ac1ba13888fab46
Marco Arcoria <marcoarco03@gmail.com>
2025-12-02 22:04:11 +0100

    aggiungo un main java

---
+++ miaproof/src/Uno/main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Uno;
+
+/**
+ *
+ * @author marco
+ */
+public class main {
+    public static void main(String arg[]){
+            System.out.println("Hello world!!");
+            for(int i = 0; i<4; i++){
+                System.out.println("count: "+i);
+            }
+    }
+}
